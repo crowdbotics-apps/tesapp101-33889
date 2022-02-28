0ec85bd0856bc5c9c4b7b306048b65d4e8ced557
Crowdbotics <team@crowdbotics.com>
2022-02-28 07:42:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tesapp101_33889",
+  url: "https://tesapp101_33889.botics.co",
+  api: "https://tesapp101_33889.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
